d43099bb423c1421b6e75dabed95e19e20eeaa44
Parsimony <contact@parsimony-cms.com>
2012-05-23 16:07:51 +0200

    Enhance block query filters/sort/pagination

--- modules/core/blocks/query/adminView.php
+++ modules/core/blocks/query/adminView.php
@@ -107,7 +107,6 @@ $view = $this->getConfig('view');
 				    <?php
 				    $obj = app::getModule($module)->getEntity($model);
 				    foreach ($obj->getFields() AS $field) :
-					if (get_class($field) != \app::$aliasClasses['field_formasso']) :
 					    if (get_class($field) == 'core\fields\field_foreignkey')
 						$link = ' link="' . $module . '_' . $field->link . '"';
                                             elseif (get_class($field) == 'core\fields\field_user') 
@@ -117,7 +116,6 @@ $view = $this->getConfig('view');
 					    ?>
 		    			<div class="ellipsis property <?php echo $aliasClasses[get_class($field)]; ?>"<?php echo $link; ?>><?php echo $field->name; ?></div>
 					    <?php
-					endif;
 				    endforeach;
 				    ?>
 	    		    </div>
