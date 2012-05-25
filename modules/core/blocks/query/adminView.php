243b8a7def6ef6b5f0c0f79a32ca040160cc17be
Parsimony <contact@parsimony-cms.com>
2012-05-25 16:07:39 +0200

    Fix block query

--- modules/core/blocks/query/adminView.php
+++ modules/core/blocks/query/adminView.php
@@ -377,11 +377,12 @@ $view = $this->getConfig('view');
 	    }
 	});
 	$("#schema_sql .property[link]").each(function(i){
+            
 	    var table1 = $(this).parent().attr("table");
 	    var table2 = $(this).attr('link');
 	    if($.inArray(table1, tables) != -1 && $.inArray(table2, tables) != -1){//si ce n'est pas un link de table d'asso'
 		if(table1 != table2) {
-		    links.push(table2 + "=>" + table1);
+		    links.unshift(table2 + "=>" + table1);
 		    //tableCount[table2] = (tableCount[table2] || 0) + 1;
 		}
 	    }else if($.inArray(table1, tables) != -1 || $.inArray(table2, tables) != -1){ // si c'est in link de table d'asso'
@@ -393,6 +394,7 @@ $view = $this->getConfig('view');
 		if(tableProv.length >= 2){
 		    $.each( tableProv, function(i, val){
 			if($.inArray(val, links) == -1){
+                            alert(val);
 			    links.push(val);
 			    var cut = val.split("=>");
 			    //tableCount[cut[1]] = (tableCount[cut[1]] || 0) + 1;
@@ -404,27 +406,34 @@ $view = $this->getConfig('view');
 
 	var alreadyIncluded = [];
 	var cpt = 0;
+	boucle:
 	while( Object.keys(links).length != 0 ){
 	    for(var key in links){
-		var myTables = links[key].split("=>");
-		var type="inner join"
-		    
-		if(cpt == 0 || ($.inArray(myTables[0], alreadyIncluded) != -1 && $.inArray(myTables[1], alreadyIncluded) == -1)){
-		    var linkbefore = $('#links [name="relations[' + myTables[0] + '_' + myTables[1] + '][type]"]');
-		    if(linkbefore.length > 0) type = linkbefore.val();
-		    putLink(myTables[0],myTables[1],type);
-		    delete links[key];
-		    if(cpt == 0) alreadyIncluded.push(myTables[0]);
-		    alreadyIncluded.push(myTables[1]);
-		    cpt++;
-		}else if($.inArray(myTables[1], alreadyIncluded) != -1 && $.inArray(myTables[0], alreadyIncluded) == -1){
-		    var linkbefore = $('#links [name="relations[' + myTables[1] + '_' + myTables[0] + '][type]"]');
-		    if(linkbefore.length > 0) type = linkbefore.val();
-		    putLink(myTables[1],myTables[0],type);
-		    delete links[key];
-		    alreadyIncluded.push(myTables[0]);
-		    cpt++; 
-		}  
+                if(alreadyIncluded.length != tables.length){
+                    var myTables = links[key].split("=>");
+                    var type="inner join"
+
+                    if(cpt == 0 || ($.inArray(myTables[0], alreadyIncluded) != -1 && $.inArray(myTables[1], alreadyIncluded) == -1)){
+                        var linkbefore = $('#links [name="relations[' + myTables[0] + '_' + myTables[1] + '][type]"]');
+                        if(linkbefore.length > 0) type = linkbefore.val();
+                        putLink(myTables[0],myTables[1],type);
+                        delete links[key];
+                        if(cpt == 0) alreadyIncluded.push(myTables[0]);
+                        alreadyIncluded.push(myTables[1]);
+                        cpt++;
+                    }else if($.inArray(myTables[1], alreadyIncluded) != -1 && $.inArray(myTables[0], alreadyIncluded) == -1){
+                        var linkbefore = $('#links [name="relations[' + myTables[1] + '_' + myTables[0] + '][type]"]');
+                        if(linkbefore.length > 0) type = linkbefore.val();
+                        putLink(myTables[1],myTables[0],type);
+                        delete links[key];
+                        alreadyIncluded.push(myTables[0]);
+                        cpt++; 
+                    } else{
+                        delete links[key];
+                    }
+                }else{
+                    break boucle;
+                }
 	    }
 	}
 	$("#links").html($("#linkstransit").html());
