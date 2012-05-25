79537451c2df6bdd551d1f5a2cca8f0512ee034e
Parsimony <contact@parsimony-cms.fr>
2012-05-25 02:11:20 +0200

    Fix field date

--- modules/core/fields/field_date.php
+++ modules/core/fields/field_date.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Parsimony
  *
@@ -18,56 +19,63 @@
  * versions in the future. If you wish to customize Parsimony for your
  * needs please refer to http://www.parsimony.mobi for more information.
  *
-*  @authors Julien Gras et Benoît Lorillot
-*  @copyright  Julien Gras et Benoît Lorillot
-*  @version  Release: 1.0
+ *  @authors Julien Gras et Benoît Lorillot
+ *  @copyright  Julien Gras et Benoît Lorillot
+ *  @version  Release: 1.0
  * @category  Parsimony
  * @package core\fields
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
+
 namespace core\fields;
+
 /**
  * field_date Class 
  * Create a field_date field
- */ 
+ */
 class field_date extends \field {
 
-	/** @var string $title by default 'Date' */
-        protected $title = 'Date';
-/**
- * Build a field_date field
- * @param string $module
- * @param string $entity 
- * @param string $name 
- * @param string $type by default 'datetime'
- * @param integer $characters_max by default ''
- * @param integer $characters_min by default 0
- * @param string $label by default ''
- * @param string $text_help by default ''
- * @param string $msg_error by default invalid
- * @param string $default by default ''
- * @param bool $required by default true
- * @param string $regex by default '.*'
- * @param string $use by default 'normal'
- */ 
-        public function __construct($module, $entity, $name, $type='datetime', $characters_max='', $characters_min=0, $label='', $text_help='', $msg_error='invalid', $default='', $required=TRUE, $regex='.*', $visibility = 7,$use='normal') {
-            $this->constructor(func_get_args());
-        }
-        
-     /**
+    /** @var string $title by default 'Date' */
+    protected $title = 'Date';
+
+    /**
+     * Build a field_date field
+     * @param string $module
+     * @param string $entity 
+     * @param string $name 
+     * @param string $type by default 'datetime'
+     * @param integer $characters_max by default ''
+     * @param integer $characters_min by default 0
+     * @param string $label by default ''
+     * @param string $text_help by default ''
+     * @param string $msg_error by default invalid
+     * @param string $default by default ''
+     * @param bool $required by default true
+     * @param string $regex by default '.*'
+     * @param string $use by default 'normal'
+     */
+    public function __construct($module, $entity, $name, $type = 'datetime', $characters_max = '', $characters_min = 0, $label = '', $text_help = '', $msg_error = 'invalid', $default = '', $required = TRUE, $regex = '.*', $visibility = 7, $use = 'normal') {
+	$this->constructor(func_get_args());
+    }
+
+    /**
      * Validate field
      * @param string $value
      * @return string
      */
     public function validate($value) {
-	if(empty($value)){ // use insert & update
-	    return gmdate( 'Y-m-d H:i:s',time());
-	}else{
-	    return $value;
+	if (empty($value)) { // use insert & update
+	    return gmdate('Y-m-d H:i:s', time());
+	} else {
+	    if (preg_match("/^(\d{4})-(\d{2})-(\d{2}) ([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/", $value, $date)) {
+		if (checkdate($date[2], $date[3], $date[1])) {
+		    return $value;
+		}
+	    }
 	}
 	return FALSE;
     }
 
-    }
+}
 
 ?>
--- modules/core/fields/field_date/form_update.php
+++ modules/core/fields/field_date/form_update.php
@@ -25,8 +25,11 @@
  * @package core/fields
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
+$val = $row->getId()->value;
+$stamp = strtotime(s($value));
 ?>
-<div> 
+
+<div id="mydate<?php echo $val; ?>">
     <?php if ($this->use == 'normal') : ?>
         <label for="<?php echo $this->name ?>">
 	    <?php echo $this->label ?>
@@ -34,12 +37,51 @@
 		<span class="tooltip ui-icon ui-icon-info" data-tooltip="<?php echo t($this->text_help) ?>"></span>
 	    <?php endif; ?>
         </label>
-        <input type="date" class="date" name="<?php echo $this->name ?>" id="<?php echo $this->name ?>" value="<?php echo s($value) ?>" />
-        <?php echo t('at') ?>  <input type="text" id="hh" name="hh" size="2" maxlength="2">
-        : <input type="text" id="mn" name="mn" size="2" maxlength="2">
-
+        <div>
+	    <?php
+	    $locale = \app::$request->getLocale();
+	    $lang = '<input type="text" class="datesql adddd" style="width: 25px;" value="' . date('d', $stamp) . '" />';
+	    $m = date('m', $stamp);
+	    $select = ' <select type="text" class="datesql addmm" style="vertical-align: top;height: 28px;width: 70px;font-size: 13px;">';
+	    $month = array('01' => t('Jan', false), '02' => t('Feb', false), '03' => t('Mar', false), '04' => t('Apr', false), '05' => t('May', false), '06' => t('Jun', false), '07' => t('Jul', false), '08' => t('Aug', false), '09' => t('Sep', false), '10' => t('Oct', false), '11' => t('Nov', false), '12' => t('Dec', false));
+	    foreach ($month as $key => $month) {
+		if ($key == $m)
+		    $select .= '<option value="' . $key . '" selected="selected">' . $key . '-' . $month . '</option>';
+		else
+		    $select .= '<option value="' . $key . '">' . $key . '-' . $month . '</option>';
+	    }
+	    $select .='</select> ';
+	    if ($locale == 'fr_FR')
+		echo $lang . $select;
+	    else
+		echo $select . $lang . ',';
+	    ?>
+    	<input type="text" class="datesql addyyyy" style="width: 40px;" pattern="^[12][0-9]{3}$" value="<?php echo date('Y', $stamp); ?>" />
+    	@ <input class="datesql addhour" type="text" style="width: 25px;" pattern="(?:([01]?[0-9]|2[0-3]):)?([0-5][0-9])" value="<?php echo date('H', $stamp); ?>"> : 
+    	<input type="text" class="addminut datesql" style="width: 25px;" maxlength="2" pattern="(?:([01]?[0-9]|2[0-3]):)?([0-5][0-9])" value="<?php echo date('i', $stamp); ?>">
+    	<input type="hidden" class="addsecond datesql" pattern="(?:([01]?[0-9]|2[0-3]):)?([0-5][0-9])" value="<?php echo date('s', $stamp); ?>">
+        </div>
+        <input type="hidden" class="datestatus" name="<?php echo $this->name ?>">
     <?php elseif ($this->use == 'creation') : ?>
         <input type="hidden" name="<?php echo $this->name ?>" value="<?php echo s($value) ?>">
     <?php elseif ($this->use == 'update') : ?>
     <?php endif; ?>
 </div>
+<script>
+    
+    if(typeof lead != 'function'){
+	function lead(val, length) {
+	    var val = val + '';
+	    while (val.length < length)  val = '0' + val;
+	    return val;
+	}
+    }
+    
+    var myForm = $("#mydate<?php echo $val; ?>").closest("form");
+    $(myForm).on('change','.datesql', function(e) { 
+	var sqltime = lead($('.addyyyy', myForm).val(),4) + '-' + lead($('.addmm', myForm).val(),2) + '-' + lead($('.adddd', myForm).val(),2) + ' ' + lead($('.addhour', myForm).val(),2) + ':' + lead($('.addminut', myForm).val(),2) + ':' + lead($('.addsecond', myForm).val(),2);
+	$('.datestatus', myForm).val(sqltime);           
+    });
+    $('.datesql',myForm).trigger('change');
+
+</script>
