4feb0ce9f07965a07647e19dd2126d6e51cf4c78
Parsimony <contact@parsimony-cms.com>
2012-06-14 15:16:26 +0200

    Enhance role/user model

--- modules/core/model/user.php
+++
@@ -1,30 +0,0 @@
-<?php
-namespace core\model;
-/**
-* Description of entity user
-* @author Parsimony
-* @top 302px
-* @left 1427px
-*/
-class user extends \entity {
-
-    protected $id_user;
-    protected $pseudo;
-    protected $mail;
-    protected $pass;
-    protected $id_role;
-
-
-public function __construct(\field_ident $id_user,\field_string $pseudo,\field_mail $mail,\field_password $pass,\field_foreignkey $id_role) {
-        $this->id_user = $id_user;
-        $this->pseudo = $pseudo;
-        $this->mail = $mail;
-        $this->pass = $pass;
-        $this->id_role = $id_role;
-
-}
-// DON'T TOUCH THE CODE ABOVE ##########################################################
-
-}
-
-?>
