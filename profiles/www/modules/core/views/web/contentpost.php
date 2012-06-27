710e3e9c206a8864d536e0cf548e159c026122b4
Parsimony <contact@parsimony-cms.com>
2012-06-27 18:15:09 +0200

    Delete profiles files

--- profiles/www/modules/core/views/web/contentpost.php
+++
@@ -1,9 +0,0 @@
-
-<?php foreach ($view as $key => $line) : ?>
-	<div class="clearboth howtos">
-		<div class="title howtostitle"><a style="text-decoration:none;font-size:22px;color: rgba(255, 255, 255, 0.347656);line-height: 30px;text-shadow: -2px -2px 0px #0070A1; font-weight:bold" href="<?php echo BASE_PATH.$line->url; ?>"><?php echo $line->title; ?></a></div>
-		<div class="content howtosdescript"><?php echo $line->content; ?></div>
-
-	</div>
-<?php endforeach; ?>
-
