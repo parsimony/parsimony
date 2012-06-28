1cf483ca1cb66ef5b1d8d3ed343ad57556abb031
Parsimony <contact@parsimony-cms.com>
2012-06-28 11:53:28 +0200

    add profiles files in core

---
+++ modules/core/themes/views/web/error_404.php
@@ -0,0 +1,1 @@
+<h1>Put your content in this area</h1>
---
+++ modules/core/themes/views/web/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--[if ie]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <script type="text/javascript">
+        document.createElement('header');
+        document.createElement('hgroup');
+        document.createElement('nav');
+        document.createElement('menu');
+        document.createElement('section');
+        document.createElement('article');
+        document.createElement('aside');
+        document.createElement('footer');
+        </script>
+        <![endif]-->
+        <meta charset="utf-8">
+        <title><?php echo app::$request->page->getTitle() ?></title>
+        <script type="text/javascript">
+	    var BASE_PATH = '<?php echo BASE_PATH ?>';
+	    var MODULE = '<?php echo MODULE ?>';
+	    var THEME = '<?php echo THEME ?>';
+	    var THEMETYPE = '<?php echo THEMETYPE ?>';
+	    var THEMEMODULE = '<?php echo THEMEMODULE ?>';
+	    var TOKEN = '<?php echo TOKEN ?>';
+	</script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="' + BASE_PATH + 'lib/jquery-1.7.2.min.js"><\/script>')</script>
+        <meta name="generator" content="Parsimony">
+	<?php echo app::$request->page->printMetas() ?>
+        <?php echo app::$request->page->printInclusions() ?>
+	<?php echo app::$request->page->head ?>
+    </head>
+    <body>
+	<?php echo $this->body; ?>
+        
+    </body>
+</html>
