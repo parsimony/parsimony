c735ecd50ef7a3b0ba260506ac56a7577600a5cd
Benoit Parsimony <contact@parsimony-cms.com>
2012-07-04 22:57:38 +0200

    Enhance dbdesigner

---
+++ modules/core/blocks/recentposts/block.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Parsimony
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@parsimony.mobi so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Parsimony to newer
+ * versions in the future. If you wish to customize Parsimony for your
+ * needs please refer to http://www.parsimony.mobi for more information.
+ *
+ * @authors Julien Gras et Benoît Lorillot
+ * @copyright  Julien Gras et Benoît Lorillot
+ * @version  Release: 1.0
+ * @category  Blog
+ * @package core/blocks
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace core\blocks;
+
+/**
+ * Recentposts Block Class 
+ * Manages Recentposts Block
+ */
+class recentposts extends \block {
+
+    protected $category = 'blog';
+
+}
+
+?>
