18180e05a8228bcb75de6ef14afdca955e848465
Parsimony <contact@parsimony-cms.com>
2012-07-06 11:57:05 +0200

    Enhance image block

--- modules/core/blocks/image/view.php
+++ modules/core/blocks/image/view.php
@@ -28,7 +28,10 @@
 \app::$request->page->addJSFile(BASE_PATH . 'lib/fancybox/jquery.fancybox-1.3.4.js');
 \app::$request->page->addCSSFile(BASE_PATH . 'lib/fancybox/jquery.fancybox-1.3.4.css');
 ?>
-
-<a <?php if($this->getConfig('fancybox')=="1") echo 'class="fancybox"' ?> href="<?php if($this->getConfig('url')){ echo $this->getConfig('url');} else echo PROFILE_PATH.$this->getConfig('imgPath'); ?>">
-    <img  title="<?php echo $this->getConfig('title'); ?>" src="<?php echo BASE_PATH.$this->getConfig('imgPath'); ?>" alt="<?php echo $this->getConfig('alt'); ?>" style="box-sizing:border-box;width:<?php if($this->getConfig('width')){echo $this->getConfig('width').'px';}else echo '100%' ?>;<?php if($this->getConfig('height')){echo 'height:'.$this->getConfig('height').'px';}  ?>;" />
-</a>
+<?php if($this->getConfig('fancybox') == '1' || $this->getConfig('url')): ?>
+    <a <?php if($this->getConfig('fancybox')=="1") echo 'class="fancybox"' ?> href="<?php if($this->getConfig('url')){ echo $this->getConfig('url');} else echo PROFILE_PATH.$this->getConfig('imgPath'); ?>">
+        <img  title="<?php echo $this->getConfig('title'); ?>" src="<?php echo BASE_PATH.$this->getConfig('imgPath'); ?>" alt="<?php echo $this->getConfig('alt'); ?>" style="box-sizing:border-box;width:<?php if($this->getConfig('width')){echo $this->getConfig('width').'px';}else echo '100%' ?>;<?php if($this->getConfig('height')){echo 'height:'.$this->getConfig('height').'px';}  ?>;" />
+    </a>
+<?php else: ?>
+    <img title="<?php echo $this->getConfig('title'); ?>" src="<?php echo BASE_PATH.$this->getConfig('imgPath'); ?>" alt="<?php echo $this->getConfig('alt'); ?>" style="box-sizing:border-box;width:<?php if($this->getConfig('width')){echo $this->getConfig('width').'px';}else echo '100%' ?>;<?php if($this->getConfig('height')){echo 'height:'.$this->getConfig('height').'px';}  ?>;" />
+<?php endif; ?>
