d38c0903f82394e178292bfcdedf4f7bcd4c8e25
Benoit Parsimony <contact@parsimony-cms.com>
2012-07-18 12:06:52 +0200

    Enhance fr

--- modules/admin/locale/fr_FR.php
+++ modules/admin/locale/fr_FR.php
@@ -271,6 +271,7 @@ $lang['Translation']= 'Traduction';
 $lang['Translate']= 'Traduire';
 $lang['Go to the link']= 'Allez au lien';
 $lang['Error in your DOM, perhaps an HTML tag isn\'t closed.']= 'Erreur dans votre DOM, une balise HTML n\'est peut-être pas fermée.';
+$lang['Pattern']= 'Modèle';
 
 
 ?>
