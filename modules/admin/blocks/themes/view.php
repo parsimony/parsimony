d806dc2acd6a10b84d2ea0e074cd235996d6b419
Benoit Parsimony <contact@parsimony-cms.com>
2012-07-18 12:08:23 +0200

    Enhance css themes

--- modules/admin/blocks/themes/view.php
+++ modules/admin/blocks/themes/view.php
@@ -37,17 +37,9 @@ background-image: linear-gradient(top, white, #ECECEC);
 text-shadow: white 0 1px 0;display:block;border-top: 2px solid #999;}
     .adminzone .adminzonemenu{display: block;min-height: initial;width: initial}
     .adminzone .adminzonecontent{display: block;width:100%;box-shadow: -2px 0px 2px #999;background: #EEE url(admin/img/concrete_wall_3.png);}
-/*    .adminzone .adminzonemenu .adminzonetab .firstpanel{border-right: 1px solid #D3D5DB;
-}*/
     .adminzone .adminzonemenu .adminzonetab{border-left: 1px solid white;border-right: 1px solid #D3D5DB;float: left;border-top: none;border-bottom: none;}
-    .adminzone .adminzonemenu .adminzonetab a{padding: 0 10px;color: #777; background: none;border-top: none;
-border-bottom:none;
-color: #464646;
-text-decoration: none;line-height:25px;
-/*width: none*/}
-
-    .adminzone .adminzonemenu .adminzonetab a.active, .adminzonemenu .adminzonetab a:hover{border-top: none;background: #E4E4E4;
-        /*background: #777;border-bottom: none*/}
+    .adminzone .adminzonemenu .adminzonetab a{padding: 0 10px;color: #777; background: none;border-top: none;border-bottom:none;color: #464646;text-decoration: none;line-height:25px;}
+    .adminzone .adminzonemenu .adminzonetab a.active, .adminzonemenu .adminzonetab a:hover{border-top: none;background: #E4E4E4;}
     .placeholder {position: relative;clear: both;width: 200px;}
     input{width: 90px;}
     #themelist{overflow-x: scroll;overflow-y: hidden;height: 145px;width: 99.5%;}
