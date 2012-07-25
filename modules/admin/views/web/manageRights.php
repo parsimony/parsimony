69562ea3b6aa3e0569cc43afe6170342161dfb13
Benoit Parsimony <contact@parsimony-cms.com>
2012-07-25 16:22:25 +0200

    enhance manage rights

--- modules/admin/views/web/manageRights.php
+++ modules/admin/views/web/manageRights.php
@@ -32,7 +32,7 @@ $role = app::getModule('core')->getEntity('role');
     td{padding:4px 2px 7px 7px;text-align: center !important}
     .active{background:#AAA}  
     .modulename{font-size: 13px;color: #777;letter-spacing: 2px;}
-    #enablemodule{width: 300px; position: relative;top: -22px;left: 300px;font-size: 13px;color: #777;letter-spacing: 2px;}
+    #enablemodule{margin: 10px 0px;color: #464646;padding: 3px 7px;font-size: 14px;position: relative;letter-spacing: 2px;}
     .firsttd{font-size: 18px;letter-spacing: 2px;vertical-align: middle}
     .secondtd{text-transform: capitalize;color: #444;font-size: 12px;text-align:left !important}
 </style>
@@ -53,23 +53,40 @@ $role = app::getModule('core')->getEntity('role');
             <?php foreach ($role->select() as $key => $line) : ?>
                 <div id="tabs-<?php echo $line->name; ?>" class="admintabs">
                     <div>
-                        <h2><?php echo $line->name; ?></h2>
-                        <label for="type">Anonymous </label><input type="radio" name="type[<?php echo $line->id_role; ?>]" value="0" <?php if($line->state == "0") echo 'checked="checked"';  ?> />
-                        <label for="type">Edit </label><input type="radio" name="type[<?php echo $line->id_role; ?>]" value="1" <?php if($line->state == "1") echo 'checked="checked"';  ?> />
-                        <label for="type">Creation </label><input type="radio" name="type[<?php echo $line->id_role; ?>]" value="2" <?php if($line->state == "2") echo 'checked="checked"';  ?> />
-                        <br><br>
-                        <div style="clear:both"></div>
-                        <label class="modulename"><?php echo t('Module', FALSE); ?> :</label>
+                        <h2 style="letter-spacing: 1.5px;color: #555;text-transform: capitalize;margin-left: 10px;"><?php echo t('%s role', array($line->name)); ?></h2>
+                        <table style="margin-left: 10px">
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th style="width: 120px;">Anonymous<span class="tooltip ui-icon ui-icon-info floatright" data-tooltip="<?php echo t('The only right of reading content') ;?>"></span></th>
+                                    <th style="width: 120px;">Editor<span class="tooltip ui-icon ui-icon-info floatright" data-tooltip="<?php echo t('The right of editing content & pages') ;?>"></span></th>
+                                    <th style="width: 120px;">Developer<span class="tooltip ui-icon ui-icon-info floatright" data-tooltip="<?php echo t('All web development rights : Design, Module, blocks, database & so on') ;?>"></span></th></tr>
+                            </thead>
+                            <tbody>
+                                <tr>  
+                                    <td style="width: 160px;">Status of <span style="text-transform: capitalize"><?php echo $line->name; ?></span></td>
+                                    <td style="height: 40px;"><input type="radio" name="type[<?php echo $line->id_role; ?>]" value="0" <?php if($line->state == "0") echo 'checked="checked"';  ?> /></td>
+                                    <td style="height: 40px;"><input type="radio" name="type[<?php echo $line->id_role; ?>]" value="1" <?php if($line->state == "1") echo 'checked="checked"';  ?> /></td>
+                                    <td style="height: 40px;"><input type="radio" name="type[<?php echo $line->id_role; ?>]" value="2" <?php if($line->state == "2") echo 'checked="checked"';  ?> /></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                       <br>
+                       </div>
+                   
+                       <div style="clear:both"></div> 
+                       <fieldset style="background: #F9F9F9;border: 1px solid #CCC;margin: 10px;border-radius: 8px;padding-bottom: 10px;">
+                           <legend style="display: block;text-transform: capitalize;margin: 4px 7px 0px 5px;color: #464646;padding: 3px 7px;font-size: 14px;border: 1px solid #DFDFDF;border-radius: 5px;background-color: #F1F1F1;
+background-image: -ms-linear-gradient(top,#F9F9F9,#ECECEC);background-image: -moz-linear-gradient(top,#F9F9F9,#ECECEC);background-image: -o-linear-gradient(top,#F9F9F9,#ECECEC);background-image: -webkit-gradient(linear,left top,left bottom,from(#F9F9F9),to(#ECECEC));background-image: -webkit-linear-gradient(top,#F9F9F9,#ECECEC);
+background-image: linear-gradient(top,#F9F9F9,#ECECEC);"><label class="modulename"><?php echo t('Module', FALSE); ?> :</label>
                         <select name="module" onchange="$(this).closest('.admintabs').find('.rightbox').hide();$('#rights-<?php echo $line->name ?>-' + this.value).show()">
                             <?php
-                            $modules = \app::$activeModules;
-                            unset($modules['admin']);
-                            foreach ($modules as $moduleName => $type) {
+                            foreach (\app::$activeModules as $moduleName => $type) {
                                 echo '<option value="' . $moduleName . '">' . $moduleName . '</option>';
                             }
                             ?>
                         </select>
-                    </div>
+                    </legend>
                     <?php
                     foreach (\app::$activeModules as $moduleName => $type) {
                         echo '<div id="rights-' . $line->name . '-' . $moduleName . '" class="rightbox';
@@ -77,9 +94,10 @@ $role = app::getModule('core')->getEntity('role');
                             echo ' none';
                         echo '">';
                         ?>
-                        <div id="enablemodule"><?php echo '<label>' . t('Enable', FALSE) . ' ' . ucfirst($moduleName); ?> ?</label><input type="hidden" name="modulerights[<?php echo $line->id_role; ?>][<?php echo $moduleName; ?>]" value="0">
+                           <div id="enablemodule"><label><?php echo t('Enable the %s module for %s role', array(ucfirst($moduleName), $line->name)) ;?> ?</label><input type="hidden" name="modulerights[<?php echo $line->id_role; ?>][<?php echo $moduleName; ?>]" value="0">
                             <input type="checkbox" name="modulerights[<?php echo $line->id_role; ?>][<?php echo $moduleName; ?>]" <?php if (\app::getModule($moduleName)->getRights($line->id_role)) echo 'checked'; ?>></div>
-                        <table>
+                        
+                    <table style="margin: 0 auto">
                             <thead>
                                 <tr><th></th><th><?php echo t('Name', FALSE); ?></th><th><?php echo t('Display', FALSE); ?></th><th><?php echo t('Insert', FALSE); ?></th><th><?php echo t('Update', FALSE); ?></th><th><?php echo t('Delete', FALSE); ?></th></tr>
                             </thead>
@@ -138,6 +156,8 @@ $role = app::getModule('core')->getEntity('role');
                             </tbody>
                         </table>
                         <br>
+                        <div class="clearboth">
+                        
                         <?php /*
                         <table style="width:100%">
                             <thead>
@@ -163,7 +183,7 @@ $role = app::getModule('core')->getEntity('role');
                     <?php
                 }
                 ?>
-        </div>
+        </fieldset></div>
         <?php
     endforeach;
     ?>
