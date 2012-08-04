fc6a7a93adfad840b12049e647e0daa32197a6dc
Parsimony <contact@parsimony-cms.fr>
2012-08-05 01:13:13 +0200

    Enhance possibilites to style blocks v2

--- modules/core/blocks/formadd/script.js
+++ modules/core/blocks/formadd/script.js
@@ -1,14 +1,15 @@
-function block_recentposts() {
+function block_formadd() {
     
     block.call(this);  
     
-    this.name = "recentposts";
+    this.name = "formadd";
 
     this.stylableElements = {
 	"form":"form",
 	"form parts":"form div",
 	"labels":"label",
 	"inputs":"input",
+	"textareas":"textarea",
 	"selects":"select",
 	"submit button":".submit",
 	"notification":".notify",
