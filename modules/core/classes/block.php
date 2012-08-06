692856d6e4b4edd2ba6f30608c04849d4b2bbcfb
Julien Parsimony <contact@parsimony-cms.com>
2012-08-06 17:56:41 +0200

    Add header title in general configs of blocks v2

--- modules/core/classes/block.php
+++ modules/core/classes/block.php
@@ -213,7 +213,7 @@ abstract class block {
                 $balise = 'div';
             $html = '<' . $balise . ' id="' . $this->id . '" class="block '.$this->blockName.' ' . (string) $this->getConfig('cssClasses') . '">';
             if ($this->getConfig('headerTitle')) {
-                    $html .= '<h2 class="parsiTitle">'.$this->getConfig('headerTitle').'</h2>';
+                    $html .= '<h2 class="parsiTitle">'.t($this->getConfig('headerTitle')).'</h2>';
             }
             if ($this->getConfig('ajaxReload')) {
                 \app::$request->page->head .= '<script>$(document).ready(function(){setInterval("loadBlock(\'' . MODULE . '\', \'' . \app::$request->page->getId() . '\', \'' . $this->id . '\')", ' . $this->getConfig('ajaxReload') . '000);});</script>';
