1c83c345925c4416ffd107b5922cc43c6b6080b3
Benoit Parsimony <contact@parsimony-cms.com>
2012-08-17 10:52:03 +0200

    Tablet and Mobile Views

---
+++ modules/core/views/mobile/lastposts.php
@@ -0,0 +1,19 @@
+<?php if (!$view->isEmpty()) : ?>
+	<?php foreach ($view as $key => $line) : ?>
+		<div class="itemscope">		
+			<div class="itemprop publicationGMT" style="float:left"><?php echo strftime('%b %d %Y', strtotime($line->publicationGMT));?></div>		
+		<div style="display:inline">
+			<div class="itemprop title"><a href="<?php echo BASE_PATH.$line->url; ?>"><?php echo $line->title /*->displayEditInline($line) */; ?></a></div>
+			<div class="itemprop content"><?php  echo $line->content; ?></div>
+		</div>
+			<div style="display:inline" class="itemprop author"><?php echo t('By').' '.$line->pseudo; ?></div>
+			
+			
+			
+		</div>
+	<?php endforeach; ?>
+<?php else: ?>
+	<div class="noResults"><?php echo t('No results'); ?></div>
+<?php endif; ?>
+
+<?php $view->getPagination(); ?>
---
+++ modules/core/views/tablet/tabposts.php
@@ -0,0 +1,26 @@
+<script>
+$('#toolheader').on('click','.headermenu',function(){ 
+	$('#sstabmenu').html('<ul>'+$('ul',this).html()+'</ul>').css('display','inline-block'); 
+});   
+</script>
+<?php if (!$view->isEmpty()) : ?>
+	<?php foreach ($view as $key => $line) : ?>
+		<div class="itemscope">		
+			<div class="itemprop publicationGMT" style="float:right"><?php echo strftime('%b %d %Y', strtotime($line->publicationGMT));?></div>		
+		<div style="display:inline">
+			<div class="itemprop title"><a href="<?php echo BASE_PATH.$line->url; ?>"><?php echo $line->title /*->displayEditInline($line) */; ?></a></div>
+			
+			<div class="itemprop content"><?php  echo $line->content; ?></div>
+			<div style="display:inline" class="itemprop author"><?php echo t('By').' '.$line->pseudo; ?></div>
+		</div>
+			
+			
+			
+			
+		</div>
+	<?php endforeach; ?>
+<?php else: ?>
+	<div class="noResults"><?php echo t('No results'); ?></div>
+<?php endif; ?>
+
+<?php $view->getPagination(); ?>
