b90fe4d697e0103ca5edd655e6a981a51b9e3c66
Parsimony <contact@parsimony-cms.fr>
2012-09-08 01:12:48 +0200

    Update block icons

--- modules/admin/blocks/blocks/view.php
+++ modules/admin/blocks/blocks/view.php
@@ -55,7 +55,7 @@ $arrayScript = array();
 			    if (!isset($blocksCat[$categBlock]))
 				$blocksCat[$categBlock] = '';
                             if(isset($props['title'])) $title = $props['title'];
-			    $blocksCat[$categBlock] .= '<div class="admin_core_block tooltip" data-tooltip="' . ucfirst($blockName).' '.$title. '" draggable="true" id="' . $blockClassName . '" style="float:left;position:relative;background:url(' . BASE_PATH . 'modules/' . $moduleobj->getName() . '/blocks/' . $blockName . '/img.gif) center center;"></div>';
+			    $blocksCat[$categBlock] .= '<div class="admin_core_block tooltip" data-tooltip="' . ucfirst($blockName).' '.$title. '" draggable="true" id="' . $blockClassName . '" style="float:left;position:relative;background:url(' . BASE_PATH . 'modules/' . $moduleobj->getName() . '/blocks/' . $blockName . '/icon.png) center center;"></div>';
 			}
 		    }
                     if(is_file('modules/'.$moduleobj->getName() . '/blocks/' . $blockName . '/script.js')){
