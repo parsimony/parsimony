3a06ec810cb4405e60eb2af1b0ab11b51a62952b
Parsimony <contact@parsimony-cms.fr>
2013-02-15 18:25:59 +0100

    2.5

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ include('install.php');exit;
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-define('PARSIMONY_VERSION','2.4');
+define('PARSIMONY_VERSION','2.5');
 
 include('modules/core/classes/app.php');
 spl_autoload_register('\core\classes\app::autoLoad');
