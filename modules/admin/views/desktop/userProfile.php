21dfeb75a6b2d7f586f6061fc6f5275bffd6741f
Parsimony <contact@parsimony-cms.com>
2013-07-01 18:24:43 +0200

    Enhance admin profile

--- modules/admin/views/desktop/userProfile.php
+++ modules/admin/views/desktop/userProfile.php
@@ -25,7 +25,6 @@
  * @package admin
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-$role = app::getModule('core')->getEntity('role');
 ?>
 <div class="adminzone" id="admin_profile" style="padding-left: 0px;">
     <div class="adminzonecontent">
