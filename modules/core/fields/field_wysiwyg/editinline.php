4deaca2b7638c9184ccb4525f8bd292fab987b1c
Parsimony <contact@parsimony-cms.com>
2013-07-24 17:04:56 +0200

    Enhance wysiwyg

--- modules/core/fields/field_wysiwyg/editinline.php
+++ modules/core/fields/field_wysiwyg/editinline.php
@@ -1,6 +1,8 @@
 <?php
-\app::$request->page->addJSFile('lib/HTML5editor/HTML5editor.js');
-\app::$request->page->addCSSFile('lib/HTML5editor/HTML5editor.css');
+if($_SESSION['behavior'] === 0){
+	\app::$request->page->addJSFile('lib/HTML5editor/HTML5editor.js');
+	\app::$request->page->addCSSFile('lib/HTML5editor/HTML5editor.css');
+}
 \app::$request->page->addJSFile('core/fields/field_wysiwyg/script.js');
 ?>
-<div class="parsieditinline fieldwysiwyg" spellcheck="false" data-click="fieldwysiwyg" data-wysiwygplugins="<?php echo str_replace(',','","',$this->wysiwygModules); ?>" data-module="<?php echo $this->module; ?>" data-entity="<?php echo $this->entity; ?>" data-property="<?php echo $this->name; ?>" data-id="<?php echo $row->getId()->value; ?>"><?php echo $this->value; ?></div>
+<div class="parsieditinline fieldwysiwyg" spellcheck="false" data-click="fieldwysiwyg" data-wysiwygplugins="<?php echo $this->wysiwygModules; ?>" data-module="<?php echo $this->module; ?>" data-entity="<?php echo $this->entity; ?>" data-property="<?php echo $this->name; ?>" data-id="<?php echo $row->getId()->value; ?>"><?php echo $this->value; ?></div>
