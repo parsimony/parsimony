4656b1a5524e7163df6c1eca2cc7c93efe806afe
Parsimony <contact@parsimony-cms.com>
2014-05-30 15:18:38 +0200

    Fix reorderPages

--- modules/admin/blocks/modules/block.js
+++ modules/admin/blocks/modules/block.js
@@ -75,7 +75,7 @@ function blockAdminModules() {
 					if (this.id)
 						list.push(this.id);
 				});
-				$.post(BASE_PATH + "admin/reorderPages", {TOKEN:TOKEN, module: $(dragInfos.list).data("module"), order: list}, function(data) {
+				$.post(BASE_PATH + "admin/reorderPages", {TOKEN:TOKEN, module: $(dragInfos.list).parent(".moduleTab").data("module"), order: list}, function(data) {
 					ParsimonyAdmin.notify(t("The changes have been saved"), "positive");
 				});
 			});
